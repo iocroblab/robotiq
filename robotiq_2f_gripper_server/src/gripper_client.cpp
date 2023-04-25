4810161b7c88c5682a7edcac95ca5235de6f3003
Jan Rosell <jan.rosell@upc.edu>
2023-04-25 16:31:11 +0200

    modified gripper client behavior to wait the input from the user

--- robotiq_2f_gripper_server/src/gripper_client.cpp
+++ robotiq_2f_gripper_server/src/gripper_client.cpp
@@ -33,39 +33,51 @@ int main(int argc, char **argv)
 
   std::cout<<"\nCalling Gripper services..." << std::endl;
 
+  std::cout<<"\nCalling RESET..." << std::endl;
+  std_srvs::Empty::Request vreq;
+  std_srvs::Empty::Response vres;
+  ros::service::waitForService("gripper/reset", ros::Duration(5));
+  gripperResetClient.call(vreq,vres);
+  ros::Duration(1.0).sleep();
+
+  std::cout<<"\nCalling ACTIVATE..." << std::endl;
+  gripperActivateClient.call(vreq,vres);
+  ros::Duration(1.0).sleep();
+
+  int signe = -1;
+
   while(ros::ok()){
-    std::cout<<"\nCalling RESET..." << std::endl;
-    std_srvs::Empty::Request vreq;
-    std_srvs::Empty::Response vres;
-    ros::service::waitForService("gripper/reset", ros::Duration(5));
-    gripperResetClient.call(vreq,vres);
-    ros::Duration(1.0).sleep();
 
-    std::cout<<"\nCalling ACTIVATE..." << std::endl;
-    gripperActivateClient.call(vreq,vres);
-    ros::Duration(1.0).sleep();
+      //Wait for user to press a key
+      std::cout<<"\nPRESS a key to start moving the gripper" << std::endl;
+      std::cin.get();
+      if(signe == -1) {
+          std::cout<<"\nCalling CLOSE..." << std::endl;
+          gripperCloseClient.call(vreq,vres);
+          ros::Duration(1.0).sleep();
+          signe = 1;
+      }
+      else{
+          std::cout<<"\nCalling OPEN..." << std::endl;
+          gripperOpenClient.call(vreq,vres);
+          ros::Duration(1.0).sleep();
+          signe = -1;
+      }
 
+      std::cout<<"\nCalling STATUS..." << std::endl;
+      robotiq_2f_gripper_server::Status::Request sreq;
+      robotiq_2f_gripper_server::Status::Response sres;
+      gripperStatusClient.call(sreq,sres);
+      ROS_INFO_STREAM(sres.gripperstatus);
+      ros::Duration(1.0).sleep();      
+/*
     std::cout<<"\nCalling MOVE..." << std::endl;
     robotiq_2f_gripper_server::Move::Request mreq;
     robotiq_2f_gripper_server::Move::Response mres;
     mreq.apperture = 100;
     gripperMoveClient.call(mreq,mres);
     ros::Duration(1.0).sleep();
-
-    std::cout<<"\nCalling CLOSE..." << std::endl;
-    gripperCloseClient.call(vreq,vres);
-    ros::Duration(1.0).sleep();
-
-    std::cout<<"\nCalling OPEN..." << std::endl;
-    gripperOpenClient.call(vreq,vres);
-    ros::Duration(1.0).sleep();
-
-    std::cout<<"\nCalling STATUS..." << std::endl;
-    robotiq_2f_gripper_server::Status::Request sreq;
-    robotiq_2f_gripper_server::Status::Response sres;
-    gripperStatusClient.call(sreq,sres);
-    ROS_INFO_STREAM(sres.gripperstatus);
-    ros::Duration(1.0).sleep();
+*/
 
     ros::spinOnce();
   }
